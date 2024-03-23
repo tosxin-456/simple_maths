6e722cdc28a63895f90d278afd56ab200c27ec3a
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-03-23 19:50:48 +0100

    added the git repositp

---
+++ basic_math.js
@@ -0,0 +1,33 @@
+function addition (a,b){
+
+    return a + b; 
+
+}
+
+function subtraction (a,b){
+
+    return a - b;
+
+}
+
+function division(a,b){
+    
+    return a / b;
+
+}
+
+function multiplication (a,b){
+    
+    return a * b;
+
+}
+
+
+
+
+module.exports = {
+    addition ,
+    subtraction,
+    multiplication,
+    division
+}
